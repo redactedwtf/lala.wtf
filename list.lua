bd127f6018207184395ef2d3d7b0f6ac1ab82818
funds <pxniclol@gmail.com>
2026-06-11 03:40:34 -0400

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,6 +1,7 @@
 local games = {
     [136801880565837] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/Flick.lua",
     [90568084448279] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/OneTap.lua",
+    [85207102870777] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/OneTap.lua", -- Beginner Place
     [93466613073564] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/Scoped.lua",
     [114689356953798] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/Scoped.lua", -- Beginner Place
     [122446657157717] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/SniperArena.lua",
