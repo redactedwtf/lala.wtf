3a4cb056e0c4e81554ddcd605faa97df6277b4b1
funds <pxniclol@gmail.com>
2026-06-11 03:20:17 -0400

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,4 +1,7 @@
 local games = {
     [136801880565837] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/Flick.lua",
+    [90568084448279] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/OneTap.lua",
+    [93466613073564] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/Scoped.lua",
+    [122446657157717] = "https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/games/SniperArena.lua",
 }
 return games
