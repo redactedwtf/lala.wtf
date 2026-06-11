82e249137202d3b03df0d7f2abbfd900b9f8ec86
funds <pxniclol@gmail.com>
2026-06-11 02:53:48 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,10 @@
+local got, games = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_USERNAME/REPO/main/list.lua"))()
+end)
 
+if got then
+    local url = games[game.PlaceId]
+    if url then
+        loadstring(game:HttpGet(url))()
+    end
+end
