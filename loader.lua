658d098d38181394ef24441f0d72c8175eaf4d8f
funds <pxniclol@gmail.com>
2026-06-11 03:08:46 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
-local games = loadstring(game:HttpGet("https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/list.lua"))()
-local url   = games[game.PlaceId]
+local list = loadstring(game:HttpGet("https://raw.githubusercontent.com/redactedwtf/lala.wtf/refs/heads/main/list.lua"))()
+local url = list[game.PlaceId]
 
-if url then 
-    loadstring(game:HttpGet(url))() 
+if url then
+    loadstring(game:HttpGet(url))()
 end
